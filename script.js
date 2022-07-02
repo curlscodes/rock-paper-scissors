1ad910deead63b237f404c62ca8d36fd3c934506
curlscodes <brickleylewis@gmail.com>
2022-07-02 12:15:52 +0100

    add styles

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const pScoreEl = document.getElementById('pScore') 
 const cScoreEl = document.getElementById('cScore') 
 const winnerEl = document.getElementById('winner') 
+const winloseEl = document.getElementById('winlose')
 const roundEl = document.getElementById('round')
 
 function computerPlay() {
@@ -19,18 +20,18 @@ function game(playerMove, computerMove) {
         || computerMove === 'rock' && playerMove === 'scissors'
         || computerMove === 'scissors' && playerMove === 'paper') 
             {cCounter++;
-            roundEl.innerText = `You lose. ${computerMove} beats ${playerMove}!`}
+            winloseEl.innerText = `You lose. ${computerMove} beats ${playerMove}!`}
 
         if (computerMove === 'rock' && playerMove === 'paper'
         || computerMove === 'scissors' && playerMove === 'rock' 
         || computerMove === 'paper' && playerMove === 'scissors') 
             {pCounter++;
-            roundEl.innerText = `You win. ${playerMove} beats ${computerMove}!`}
+            winloseEl.innerText = `You win. ${playerMove} beats ${computerMove}!`}
 
         else if (computerMove === playerMove) {
-            roundEl.innerText = `Draw. You both chose ${playerMove}!`;}
+            winloseEl.innerText = `Draw. You both chose ${playerMove}!`;}
         
-    else if (pCounter == 5) {
+    if (pCounter == 5) {
         winnerEl.innerText = "You won!";
         rockBtn.disabled = true;
         paperBtn.disabled = true;
@@ -41,13 +42,15 @@ function game(playerMove, computerMove) {
         paperBtn.disabled = true;
         scissorsBtn.disabled = true;
     }
-    pScoreEl.innerText = `Player:${pCounter}`
-    cScoreEl.innerText = `Computer:${cCounter}`
-
+    pScoreEl.innerText = `Player: ${pCounter}`
+    cScoreEl.innerText = `Computer: ${cCounter}`
+    round++;
+    roundEl.innerText = `Round: ${round}`;
 }
 
 let cCounter = 0
 let pCounter = 0
+let round = 0
 
 let rockBtn = document.querySelector('#rock');
 let paperBtn = document.querySelector('#paper');
